32a85fbf8c11cc9f906b92b78d607d606d33ceca
liuflin <liuflin@bupt.edu.cn>
2024-10-25 21:16:04 +0800

    feat: 增加StatusText和Tip组件；下一步对各个UI组件进行组装，看逻辑是否有问题。

--- app/src/main/java/com/fifate/tomato_clock/ui/base/PomorotoTimerUI.kt
+++ app/src/main/java/com/fifate/tomato_clock/ui/base/PomorotoTimerUI.kt
@@ -2,6 +2,7 @@ package com.fifate.tomato_clock.ui.base
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -93,71 +94,76 @@ fun PomodoroTimerUI(
     val timeDisplay = derivedStateOf {
         String.format("%2d:%02d", remainingFocusTime / 60, remainingFocusTime % 60)
     }
-    // UI layout
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        if(isPause){
-            Text("暂停中",fontSize = 24.sp)
-        }
-        when(state){
-            PomodoroState.INIT -> Button(onClick = startFocus){
-                Text(text = "开始专注", fontSize = 24.sp)
-            }
-            PomodoroState.FOCUSING -> Column(modifier = Modifier.align(Alignment.CenterHorizontally)){
+    Box(){
 
-                Text(timeDisplay.value, fontSize = 100.sp,modifier = Modifier.align(Alignment.CenterHorizontally)) //显示倒计时
-                Row {
-                    if (!isPause){
-                        Button(onClick = pauseTimer) { Text(text = "暂停",fontSize=24.sp) }
-                        Button(onClick = skipFocus){ Text(text="跳过", fontSize=24.sp) }
-                    }else {
-                        Button(onClick = continueTimer) { Text(text = "继续") }
-                    }
-                    Button(onClick = resetTimer){Text(text="重置", fontSize=24.sp)} //重置按钮
-                }
-            }
-            PomodoroState.FOCUSED -> Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                Text(String.format("%2d:%02d", overFocusTime / 60, overFocusTime % 60), fontSize = 20.sp)
-                Text(text="专注完成，快活动活动休息一下吧！", fontSize = 20.sp,modifier = Modifier.align(Alignment.CenterHorizontally))
-                Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                    if (!isPause){
-                        Button(onClick = pauseTimer) { Text(text = "暂停",fontSize=12.sp)}
-                        Button(onClick = startBreak){ Text(text="开始休息", fontSize=12.sp) }
-                    }else {
-                        Button(onClick = continueTimer) { Text(text = "继续",fontSize=12.sp) }
-                    }
-                    Button(onClick = resetTimer){Text(text="重置", fontSize=12.sp)} //重置按钮
-                }
-            }
-            PomodoroState.BREAKING -> Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
-                Text(text="休息中", fontSize = 48.sp)
-                Text(String.format("%2d:%02d", remainingBreakTime / 60, remainingBreakTime % 60), fontSize = 48.sp)
-                Row {
-                    if (!isPause){
-                        Button(onClick = pauseTimer) { Text(text = "暂停") }
-                        Button(onClick = skipBreak){ Text(text="跳过", fontSize=24.sp) }
-                    }else {
-                        Button(onClick = continueTimer) { Text(text = "继续") }
-                    }
-                    Button(onClick = resetTimer){Text(text="重置", fontSize=24.sp)} //重置按钮
-                }
-            }
-            PomodoroState.BROKE -> Column {
-                Text(String.format("%2d:%02d", overBreakTime / 60, overBreakTime % 60), fontSize = 24.sp)
-                Text(text="休息时间结束，打起精神继续加油！", fontSize = 48.sp)
-                if (!isPause){
-                    Button(onClick = pauseTimer) { Text(text = "暂停") }
-                    Button(onClick = startFocus){ Text(text="开始专注", fontSize=24.sp) }
-                }else {
-                    Button(onClick = continueTimer) { Text(text = "继续") }
-                }
-                Button(onClick = resetTimer){Text(text="重置", fontSize=24.sp)} //重置按钮
-            }
-        }
     }
+
+
+//    // UI layout
+//    Column(
+//        modifier = Modifier.fillMaxSize(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        verticalArrangement = Arrangement.Center
+//    ) {
+//        if(isPause){
+//            Text("暂停中",fontSize = 24.sp)
+//        }
+//        when(state){
+//            PomodoroState.INIT -> Button(onClick = startFocus){
+//                Text(text = "开始专注", fontSize = 24.sp)
+//            }
+//            PomodoroState.FOCUSING -> Column(modifier = Modifier.align(Alignment.CenterHorizontally)){
+//
+//                Text(timeDisplay.value, fontSize = 100.sp,modifier = Modifier.align(Alignment.CenterHorizontally)) //显示倒计时
+//                Row {
+//                    if (!isPause){
+//                        Button(onClick = pauseTimer) { Text(text = "暂停",fontSize=24.sp) }
+//                        Button(onClick = skipFocus){ Text(text="跳过", fontSize=24.sp) }
+//                    }else {
+//                        Button(onClick = continueTimer) { Text(text = "继续") }
+//                    }
+//                    Button(onClick = resetTimer){Text(text="重置", fontSize=24.sp)} //重置按钮
+//                }
+//            }
+//            PomodoroState.FOCUSED -> Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+//                Text(String.format("%2d:%02d", overFocusTime / 60, overFocusTime % 60), fontSize = 20.sp)
+//                Text(text="专注完成，快活动活动休息一下吧！", fontSize = 20.sp,modifier = Modifier.align(Alignment.CenterHorizontally))
+//                Row(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+//                    if (!isPause){
+//                        Button(onClick = pauseTimer) { Text(text = "暂停",fontSize=12.sp)}
+//                        Button(onClick = startBreak){ Text(text="开始休息", fontSize=12.sp) }
+//                    }else {
+//                        Button(onClick = continueTimer) { Text(text = "继续",fontSize=12.sp) }
+//                    }
+//                    Button(onClick = resetTimer){Text(text="重置", fontSize=12.sp)} //重置按钮
+//                }
+//            }
+//            PomodoroState.BREAKING -> Column(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+//                Text(text="休息中", fontSize = 48.sp)
+//                Text(String.format("%2d:%02d", remainingBreakTime / 60, remainingBreakTime % 60), fontSize = 48.sp)
+//                Row {
+//                    if (!isPause){
+//                        Button(onClick = pauseTimer) { Text(text = "暂停") }
+//                        Button(onClick = skipBreak){ Text(text="跳过", fontSize=24.sp) }
+//                    }else {
+//                        Button(onClick = continueTimer) { Text(text = "继续") }
+//                    }
+//                    Button(onClick = resetTimer){Text(text="重置", fontSize=24.sp)} //重置按钮
+//                }
+//            }
+//            PomodoroState.BROKE -> Column {
+//                Text(String.format("%2d:%02d", overBreakTime / 60, overBreakTime % 60), fontSize = 24.sp)
+//                Text(text="休息时间结束，打起精神继续加油！", fontSize = 48.sp)
+//                if (!isPause){
+//                    Button(onClick = pauseTimer) { Text(text = "暂停") }
+//                    Button(onClick = startFocus){ Text(text="开始专注", fontSize=24.sp) }
+//                }else {
+//                    Button(onClick = continueTimer) { Text(text = "继续") }
+//                }
+//                Button(onClick = resetTimer){Text(text="重置", fontSize=24.sp)} //重置按钮
+//            }
+//        }
+//    }
 }
 
 
---
+++ app/src/main/java/com/fifate/tomato_clock/ui/base/Tip.kt
@@ -0,0 +1,38 @@
+package com.fifate.tomato_clock.ui.base
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Modifier
+import com.fifate.tomato_clock.state.PomodoroState
+
+@Composable
+fun Tip(modifier: Modifier,state: MutableState<PomodoroState>){
+    Surface(color = MaterialTheme.colorScheme.onSurface) {
+        val tip:String = when(state.value){
+            PomodoroState.INIT->"幽兰生前庭，含熏待清风"
+            PomodoroState.FOCUSING->"折茎聊可佩，入室自成芳"
+            PomodoroState.FOCUSED -> "玉人何处空遗佩，月落沧江壹笛秋"
+            PomodoroState.BREAKING -> "清风摇翠环，凉露滴苍玉"
+            PomodoroState.BROKE -> "两竿翠竹拂云长，几叶幽兰带露香"
+        }
+        Text(tip)
+    }
+}
+
+
+@Composable
+fun StatusText(modifier: Modifier,state: MutableState<PomodoroState>){
+    Surface(color = MaterialTheme.colorScheme.onSurface) {
+        val statusText:String = when(state.value){
+            PomodoroState.INIT->"开始专注"
+            PomodoroState.FOCUSING->"专注中"
+            PomodoroState.FOCUSED -> "开始休息"
+            PomodoroState.BREAKING -> "休息中"
+            PomodoroState.BROKE -> "开始专注"
+        }
+        Text(statusText)
+    }
+}
